544bcf87a1d537d4ab2c7c45b8488ce772808261
chetna patidar <chetnapatidar745@gmail.com>
2023-01-20 18:00:04 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const unbounce = document .querySelector(".nav-left h1");
+const mob = document .querySelector(".mob");
+unbounce .addEventListener("click",function(){
+    mob.style.transform = "translateX(10%)";
+})
